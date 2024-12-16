e89e6577bd0b398d72c026b19e4f9f89c94e3850
acky <188875536+acky-samurai@users.noreply.github.com>
2024-12-17 08:50:25 +0900

    Markup memo list screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,9 @@
+// テンプレートのようなファイル、テンプレートの中に現時点であればindex.tsxが表示される
 import { Slot } from 'expo-router'
 
 const Layout = (): JSX.Element =>{
+    // 受け取った画面をそのまま表示する
     return <Slot />
 }
-
+// reactnativeのコンポーネントを記載するときは必ずexport defaultと記載する
 export default Layout
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,67 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
-const Index = () => {
+const Index = (): JSX.Element => {
     return (
-        <View>
-            <Text>Hello World</Text>
+        <View style={styles.container}>
+            {/* Header */}
+            <View>
+                <View>
+                    <Text>Memo App</Text>
+                    <Text>ログアウト</Text>
+                </View>
+            </View>
+            {/* MemoList */}
+            <View>
+                {/* memoItem */}
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2024年12月17日 08:15</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2024年12月17日 08:15</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2024年12月17日 08:15</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+
+
+            </View>
+            {/* button */}
+            <View>
+                <Text>+</Text>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        // containerに対して画面全体を覆う
+        flex: 1,
+        // 上下左右の中央にアイテムを持ってくる
+        justifyContent: 'center',
+        // ReactNativeでは横方向の制御がalignItems
+        alignItems: 'center'
+    }
+})
 
 export default Index
