0fe6c83dfd246f4760e1b4fa71fce41afeb66d0f
acky <188875536+acky-samurai@users.noreply.github.com>
2024-12-20 08:23:43 +0900

    Styling of Header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,10 +4,10 @@ const Index = (): JSX.Element => {
     return (
         <View style={styles.container}>
             {/* Header */}
-            <View>
-                <View>
-                    <Text>Memo App</Text>
-                    <Text>ログアウト</Text>
+            <View style={styles.header}>
+                <View style={styles.headerInner}>
+                    <Text style={styles.headerTitle}>Memo App</Text>
+                    <Text style={styles.headerRight}>ログアウト</Text>
                 </View>
             </View>
             {/* MemoList */}
@@ -55,12 +55,34 @@ const Index = (): JSX.Element => {
 
 const styles = StyleSheet.create({
     container:{
-        // containerに対して画面全体を覆う
+        // containerに対して画面全体を覆う,画面いっぱいに要素を広げる,containerという要素が設定された箇所全体に適応される.
         flex: 1,
-        // 上下左右の中央にアイテムを持ってくる
-        justifyContent: 'center',
-        // ReactNativeでは横方向の制御がalignItems
-        alignItems: 'center'
+        backgroundColor: '#ffffff'
+    },
+    header:{
+        backgroundColor: '#467FD3',
+        height: 104,
+        justifyContent: 'flex-end'
+    },
+    headerInner: {
+        // 横方向の制御.
+        alignItems:'center'
+    },
+    headerRight: {
+        // 絶対位置を指定する(absolute),flexボックスのコントロールから外れて好きな位置に要素を配置できる.
+        position: 'absolute',
+        // 右方向からどのくらいの位置に配置するかを指定.
+        right: 16,
+        bottom: 16,
+        color: 'rgba(255,255,255,0.7)'
+    },
+    headerTitle: {
+        // 下に少し余白を持たせる.
+        marginBottom: 8,
+        fontSize: 22,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        color: '#ffffff'
     }
 })
 
