6a337c1522588af86d7274d3437957b05b3c2482
acky <188875536+acky-samurai@users.noreply.github.com>
2024-12-27 08:07:35 +0900

    Structure and styling of the note editing screen

---
+++ src/app/memo/edit.tsx
@@ -0,0 +1,44 @@
+import { 
+    View, TextInput, StyleSheet, KeyboardAvoidingView
+ } from 'react-native'
+
+import Header from '../../compornents/header'
+import CircleButton from '../../compornents/CircleButton'
+import Icon from '../../compornents/icon'
+
+const Edit = (): JSX.Element => {
+    return (
+        // ReactNativeの公式サイトからKeyboardAvoidingViewにbehaviorを設定することを推奨されているため設定.
+        <KeyboardAvoidingView behavior='height' style={styles.container}>
+            <Header />
+            <View style={styles.inputCOntainer}>
+                {/* multilineを入れることで、複数行のテキストと認識し、iosでもtextAlignVerticalが適用される. */}
+                {/* 改行を有効にするには中括弧でvalueを設定する. */}
+                <TextInput multiline style={styles.input} value={'買い物\nリスト'} />
+            </View>
+            <CircleButton>
+                <Icon name='check' size={40} color='#ffffff' />
+            </CircleButton>
+        </KeyboardAvoidingView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    inputCOntainer: {
+        paddingVertical: 32,
+        paddingHorizontal: 27,
+        flex: 1
+    },
+    input: {
+        flex: 1,
+        // Androidには下記でテキストを上部に持ってこれる.
+        textAlignVertical: 'top',
+        fontSize: 16,
+        lineHeight: 24
+    }
+})
+
+export default Edit
